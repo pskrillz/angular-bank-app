254ec156d26eec8621b3c564c1306d0b201f6327
pskrillz <payam.farah@live.com>
2020-05-15 14:41:44 -0700

    ngIf getting complicated, learning to implement ngSwitch

--- src/app/bank.service.ts
+++ src/app/bank.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { e } from '@angular/core/src/render3';
 
 @Injectable({
   providedIn: 'root'
@@ -42,17 +43,37 @@ export class BankService {
     ]
   };
 
+
+  // Properties dynamic display (default)
   value: number;
+
+  
   transactionGenerated = false;
   transactionHistory = false;
   showCustInfo = true;
+  greeting = true;
+  balanceView = false
+  defaultScreen = true;
 
+
+  allExeceptOneToggle(){
+    this.transactionGenerated = false;
+    this.transactionHistory = false;
+    this.showCustInfo = true;
+    this.greeting = true;
+    this.balanceView = false
+    this.defaultScreen = true;
+  }
 
 
   withdraw() {
     console.log(this.value)
     if ( this.value > this.account.balance) {
       return alert('insufficent funds!')}
+
+    if ( this.value === null || this.value === 0) {
+       return alert('No Amount Was Entered')
+    }
 
     this.account.balance -= this.value
     this.transactionGenerated = true;
@@ -69,7 +90,7 @@ export class BankService {
    
 
    deposit() {
-    if ( this.value === null) {
+    if ( this.value === null || this.value === 0) {
       return alert('No Amount Was Entered')
     }
 
@@ -89,6 +110,7 @@ export class BankService {
    homeScreen(){
      this.transactionGenerated = false;
      this.transactionHistory = false;
+     this.greeting = true
    }
 
    showTransactionHistory() {
@@ -100,6 +122,21 @@ export class BankService {
     
    }
 
+
+   showBalance() {
+    //  this.homeScreen();
+   if (this.balanceView === false) {
+        this.balanceView = true
+        this.transactionGenerated = false;
+        this.transactionHistory = false;
+        this.greeting = false;
+        this.balanceView = true
+
+   } else {
+    
+    }
+
+   }
 
 
 
